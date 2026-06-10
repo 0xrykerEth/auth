e9f2af78f5b0a3123e678cf3fd524defb1623e18
oxryker.bnb <86192937+0xrykerEth@users.noreply.github.com>
2026-06-10 14:13:16 +0530

    secuity fix on frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { Switch, Route } from 'react-router-dom';
-
+import { useContext } from 'react';
+import AuthContext from './components/Store/auth-store';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
+  const authCtrx = useContext(AuthContext)
+  const login = authCtrx.isLoggedIn;
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!login && 
+        (<Route path='/auth'>
           <AuthPage />
-        </Route>
+        </Route>)
+        }
         <Route path='/profile'>
-          <UserProfile />
+          {login && <UserProfile />}
+          {!login && <Redirect to="/auth"/>}
         </Route>
       </Switch>
     </Layout>
